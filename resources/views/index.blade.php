d8b553da563d7a57b9fcf742ca4d3045b19801d7
zulkifli-mizini <36199600+zulkifli-mizini@users.noreply.github.com>
2020-06-03 04:37:42 +0700

    update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+@section('title', 'Halaman Awal')
+    
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3"> Web Laravel</h1>
+        </div>
+    </div>
+ </div>
+
+ @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    $nama = 'zul';
+    return view('about', ['nama' => $nama]);
 });
