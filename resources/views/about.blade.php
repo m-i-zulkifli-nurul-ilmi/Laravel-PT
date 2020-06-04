62f19888beface231dafd06303d044b14de78a65
zulkifli-mizini <36199600+zulkifli-mizini@users.noreply.github.com>
2020-06-04 22:20:49 +0700

    ud

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <div class="row">
         <div class="col-10">
-            <h1 class="mt-3">Halaman About, {{$nama}}</h1>
+            <h1 class="mt-3">Halaman About {{$nama}}</h1>
         </div>
     </div>
  </div>
